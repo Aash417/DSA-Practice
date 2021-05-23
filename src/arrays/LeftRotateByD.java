e37439350620f3b07cdc121d40778ce18a57c9a1
Aashish_k <aashishkathat00@gmail.com>
2021-05-23 20:48:52 +0530

    added left rotate question

--- src/arrays/LeftRotateByD.java
+++ src/arrays/LeftRotateByD.java
@@ -3,26 +3,51 @@ package arrays;
 public class LeftRotateByD extends LeftRotateAnArray {
     public static void main(String[] args) {
 
-        int[] arr = { 10, 20, 30, 40, 50 };
+        // original arraay is rotating all the time if want to check any one part of
+        // code then comment the otheres in main
+
+        int[] arr = { 10, 20, 30, 40, 50, 60, 70 };
         System.out.println("");
-        int[] narr = lRotateByD(arr, 2);
-        System.out.println("using naive method : ");
-
-        for (int i : narr) {
-            System.out.println(i);
+        System.out.println("");
+        System.out.print("original one :  ");
+        for (int i : arr) {
+            System.out.print(i + "  ");
         }
 
-        System.out.println("using efficient method : ");
-        int[] newa = lRotateByD2(arr, 2);
-        for (int i : newa) {
-            System.out.println(i);
+        System.out.println("");
+        System.out.println("");
+        int[] tempNewArr = lRotateByD(arr, 2);
+        System.out.print("using naive method : ");
+        for (int i : tempNewArr) {
+        System.out.print(i + "  ");
         }
 
+        System.out.println("");
+        System.out.println("");
+        int[] tempNewArr2 = lRotateByD2(arr, 1);
+        System.out.print("using better method : ");
+        for (int i : tempNewArr2) {
+            System.out.print(i + "  ");
+        }
+
+        System.out.println("");
+        System.out.println("");
+        System.out.print("using efficient method : ");
+        int[] tempNewArr3 = lRotateByD3(arr, 1);
+        for (int i : tempNewArr3) {
+            System.out.print(i + "  ");
+        }
+       
+        System.out.println("");
+        System.out.print("original one :  ");
+        for (int i : arr) {
+            System.out.print(i + "  ");
+        }
     }
 
     // naive
     public static int[] lRotateByD(int arr[], int d) {
-
+        
         for (int i = 0; i < d; i++) {
             lRotate(arr);
         }
@@ -30,7 +55,7 @@ public class LeftRotateByD extends LeftRotateAnArray {
     }
 
     // efficient
-    public static int[] lRotateByD2(int arr[], int d) {
+    public static int[] lRotateByD3(int arr[], int d) {
 
         int n = arr.length;
         rev(arr, 0, d - 1);
@@ -49,5 +74,25 @@ public class LeftRotateByD extends LeftRotateAnArray {
             low++;
             high--;
         }
+    }
+
+    // better method
+    public static int[] lRotateByD2(int arr[], int d) {
+
+        int temp[] = new int[d];
+        // coping elements to new temp aarr
+        for (int i = 0; i < d; i++) {
+            temp[i] = arr[i];
+        }
+        // copying elements to new indexs
+        for (int i = d; i < arr.length; i++) {
+            arr[i - d] = arr[i];
+        }
+        int n = arr.length;
+        for (int i = 0; i < d; i++) {
+            arr[n - d - i] = temp[i];
+        }
+        return arr;
+
     }
 }
